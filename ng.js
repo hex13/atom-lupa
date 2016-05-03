f69287124d3f504fe3bf62647301c6af9fc84da0
Łukasz Lityński <hex13code@gmail.com>
2016-05-03 19:05:23 +0200

    ng example

---
+++ ng.js
@@ -0,0 +1,18 @@
+(function () {
+    angular.module('blah')
+
+        .directive('Cat', function () {
+            return {
+                restrict: 'E',
+                template: 'Hello Kitty'
+            };
+        })
+
+        .directive('Dog', function () {
+            return {
+                restrict: 'E',
+                template: 'Hello Kitty'
+            };
+        });
+
+})();
