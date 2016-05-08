3be3152a191d2395728117478fd031f8e5eadf77
Łukasz Lityński <hex13code@gmail.com>
2016-05-08 22:46:35 +0200

    Basic redux boilerplate

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-require('./lib/atom-lupa.coffee');
+"use strict";
+let env;
+const redux = require('redux');
+
+if (typeof atom != 'undefined') {
+    env = 'atom';
+}
+
+const main = ({
+    atom: require('./lib/atom-bindings/atom-main.js')
+})[env];
+
+const handlers = {
+    setActiveFile(state, action) {
+        return Object.assign({}, state, {activeFile: action.file})
+    }
+}
+
+function reducer(state, action) {
+    if (handlers.hasOwnProperty(action.type)) {
+        return handlers[action.type](state, action);
+    }
+    return state;
+}
+const store = redux.createStore(reducer);
+store.subscribe(function () {
+    console.log("SUBSCRIBE", store.getState())
+})
+
+module.exports = main(store.dispatch);
