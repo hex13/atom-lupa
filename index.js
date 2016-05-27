297f6a8f1c3a2eb84ee31ead977b81474a64bd19
Łukasz Lityński <hex13code@gmail.com>
2016-05-27 17:25:57 +0200

    move lupaMiddleware to separate file

--- index.js
+++ index.js
@@ -15,9 +15,6 @@ function patchModule([path, m]) {
     Object.keys(m).forEach(k => m[k].lupaMetadata = path);
 }
 
-// TODO SoC: index.js should not depend directly atom API
-const showMessage = s => atom.notifications.addSuccess(s);
-const openFile = f => atom.workspace.open(f);
 
 let env;
 const redux = require('redux');
@@ -27,20 +24,14 @@ import thunk from 'redux-thunk';
 const Lupa = require('./lib/components/Lupa').Lupa;
 const lupa = require('lupa');
 const analysis = lupa.analysis;
-const File = require('vinyl');
-const helpers = require('lupa/src/helpers');
+const lupaMiddleware = require('./lib/lupaMiddleware')(analysis);
+
 const fs = require('fs');
 
 
 if (typeof atom != 'undefined') {
     env = 'atom';
 }
-
-const DEFAULT_CONFIG = {
-    comment: 'This is only default config! It may not be appriopriate to your project. Check more on atom.io/packages/atom-lupa',
-    filePattern: "src/**/*.js",
-}
-const CONFIG_FILE_NAME = 'lupaProject.json';
 
 
 const main = ({
@@ -61,67 +52,6 @@ analysis.indexing.subscribe(files => {
 });
 
 
-function lupaMiddleware(store) {
-    return (next) => (action) => {
-        if (action.type == 'indexProject') {
-            const activeFile = store.getState().activeFile;
-            const activeDirectory = Path.dirname(activeFile.path);
-            if (activeFile) {
-                let configFile = helpers.findInParentDirectories(activeDirectory, CONFIG_FILE_NAME);
-                if (!configFile) {
-                    // infer most probably project root in case when configFile is not specified
-                    const gitRoot = helpers.findInParentDirectories(activeDirectory, '.git');
-                    // TODO in case where there is no `.git` directory, root should be taken from localization of:
-                    // package.json, gulpfile, gruntfile, .eslintrc, README.md etc.
-                    // maybe create some array `potentialRootIndicators` like this
-                    // ['package.json', 'Gulpfile.js', 'gruntfile.js', '.eslintrc']
-                    // and check them after match is found
-                    // in case where there is no match at all: ask user about root
-                    if (gitRoot) {
-                        configFile = Path.join(Path.dirname(gitRoot), CONFIG_FILE_NAME);
-                        console.log('creating config file...', configFile);
-                        fs.writeFileSync(configFile, JSON.stringify(DEFAULT_CONFIG, 0, 2), 'utf8');
-                        openFile(configFile);
-                        showMessage('created config file:' + configFile);
-                    }
-                }
-                if (!configFile) {
-                    alert("You have to create config file. Check documentation.");
-                } else {
-                    analysis.indexProject(configFile);
-                }
-            }
-        }
-        if (action.type == 'setActiveFile') {
-            const contents = action.file.contents;
-            const f = new File({
-                path: action.file.path,
-                contents: new Buffer(contents)
-            });
-            analysis.invalidate(f);
-            analysis.process(f).subscribe(f => {
-
-                analysis.findImporters(f.path).toArray().subscribe(function (importers) {
-                    const importersMd = importers.map(f => ({
-                        type: 'imported by',
-                        name: f.path,
-                        file: f,
-                    }))
-                    const finalMetadata = (f.metadata || []).concat(importersMd)
-                    store.dispatch({
-                        type: 'setMetadata',
-                        file: f,
-                        metadata: finalMetadata.map((ent, i) => {
-                            return Object.assign({}, ent, {id: i});
-                        })
-                    });
-                });
-            });
-
-        }
-        next(action);
-    }
-}
 
 function mapStateToProps(state) {
     return {
--- spec/atom-lupa-spec.js
+++ spec/atom-lupa-spec.js
@@ -1,12 +1,29 @@
+const cheerio = require('cheerio');
+
 describe("Atom Lupa", function () {
-    it('should fail', function () {
+    beforeEach(function () {
+        const workspaceElement = atom.views.getView(atom.workspace);
+        this.el = workspaceElement;
+        //jasmine.attachToDOM(workspaceElement)
         waitsForPromise(() => {
-            return atom.workspace.open('helloworld').then(editor => {
-                expect(true).toBe(false);
+            return atom.workspace.open(__dirname + '/../index.js').then(editor => {
             });
         });
 
+        waitsForPromise(() => {
+            const prom = atom.packages.activatePackage('atom-lupa');
+            atom.commands.dispatch(workspaceElement, 'atom-lupa:toggle');
+            return prom;
+        });
 
+        //
+    });
+    it('should pass smoke test', function () {
+        runs(() => {
+            expect(atom.packages.isPackageActive('atom-lupa')).toBe(true);
+            const $ = cheerio(this.el.innerHTML);
+            expect($.find('.lupa-structure').length).toBe(1);
+        });
 
     });
 });
