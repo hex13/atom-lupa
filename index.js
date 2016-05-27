672f13840302dd30c9c410e78fbfb5f5c5aca37e
Łukasz Lityński <hex13code@gmail.com>
2016-05-27 23:29:20 +0200

    refactoring: inject environment and env specific objects to init.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-module.exports = require('./lib/init')();
+let env;
+
+if (typeof atom != 'undefined') {
+    env = 'atom';
+}
+
+module.exports = require('./lib/init')({
+    env: env,
+    main: ({
+        atom: require('./lib/atom-bindings/atom-main.js')
+    })[env],
+    editorMiddleware: ({
+        atom: require('./lib/atom-bindings/middleware.js')
+    })[env]
+});
