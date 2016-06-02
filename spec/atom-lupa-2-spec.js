7145f0f05da449a9c6ee0d2cd9158ee3ae197854
Łukasz Lityński <hex13code@gmail.com>
2016-06-02 09:47:35 +0200

    add regression test #14 #18 #19

---
+++ spec/atom-lupa-2-spec.js
@@ -0,0 +1,37 @@
+const cheerio = require('cheerio');
+const TestUtils = require('react-addons-test-utils');
+
+
+describe("Atom Lupa when Atom Settings are open", function () {
+    beforeEach(function () {
+        const workspaceElement = atom.views.getView(atom.workspace);
+        this.el = workspaceElement;
+        waitsForPromise(() => {
+            const prom = atom.packages.activatePackage('settings-view');
+            atom.commands.dispatch(workspaceElement, 'settings-view:open');
+            return prom;
+        });
+
+        waitsForPromise(() => {
+            const prom = atom.packages.activatePackage('atom-lupa');
+            atom.commands.dispatch(workspaceElement, 'atom-lupa:toggle');
+            return prom;
+        });
+
+    });
+
+    it('should pass smoke te1st', function (done) {
+        let ip;
+        runs(()=> {
+            ip = this.el.querySelector('.lupa-index-project');
+            TestUtils.Simulate.click(ip);
+        });
+
+        waits(1500);
+
+        runs(() => {
+
+        });
+
+    });
+});
